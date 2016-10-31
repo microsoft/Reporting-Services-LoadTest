be78cc17f132c7c76d355604c6fd4a3f893253c3
Jaime Tarquino <jaimeta@microsoft.com>
2016-10-30 22:22:36 -0700

    build with nuget restore

--- build.cmd
+++ build.cmd
@@ -1,1 +1,3 @@
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "((new-object net.webclient).DownloadFile('https://nuget.org/nuget.exe', 'NuGet.exe'))"
+nuget restore
 msbuild /verbosity:minimal /target:rebuild /p:"Platform=Any CPU"
